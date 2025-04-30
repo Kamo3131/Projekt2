c8564272ae724b9447a4f27ff9b7fdedd4759aff
Kamo3131 <kacpermar2004@gmail.com>
2025-04-30 23:05:01 +0200

    new file:   CMakeLists.txt 	new file:   Main.cpp 	new file:   README.md 	new file:   include/Directory.h 	new file:   include/FSItem.h 	new file:   include/File.h 	new file:   src/Directory.cpp 	new file:   src/FSItem.cpp 	new file:   src/File.cpp

---
+++ Laby/Projekt2-main/include/FSItem.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <memory>
+class FSItem{
+private:
+std::string name;
+public:
+FSItem();
+FSItem(const std::string name);
+virtual ~FSItem();
+FSItem(const FSItem & fsitem);
+FSItem(FSItem && fsitem);
+FSItem & operator=(const FSItem & fsitem);
+FSItem & operator=(FSItem && fsitem);
+void setName(std::string name);
+std::string getName() const;
+virtual void print(std::ostream & os, int indent, const std::string & relativePath) const = 0;
+virtual std::unique_ptr<FSItem> clone() const = 0;
+};
---
+++ Laby/Projekt2-main/include/File.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "FSItem.h"
+class File final: public FSItem{
+private:
+size_t size=0;
+public:
+File(const std::string name);
+~File();
+File(const File & file);
+File(File && file);
+File & operator=(const File & file);
+File & operator=(File && file);
+
+size_t  getSize() const;
+void print(std::ostream & os, int indent, const std::string & relativePath) const;
+std::unique_ptr<FSItem> clone() const;
+};
